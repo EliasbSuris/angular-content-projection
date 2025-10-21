95d6fb089a74d25f28f2ee5beba2434c58c5279e
Elias Barreiro Suris <ebarreiro@atsistemas.com>
2025-10-21 11:31:58 +0200

    feat(multi-slot):  add multi-slot demo

--- src/app/pages/multi-slot/multi-slot.ts
+++ src/app/pages/multi-slot/multi-slot.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Container } from '@features/simple-content-projection/container/container';
+import { ProjectedContent } from '@features/simple-content-projection/projected-content/projected-content';
+import { PageTitle } from '@ui/page-title/page-title';
 
 @Component({
   selector: 'aor-multi-slot',
-  imports: [],
+  imports: [Container, PageTitle, ProjectedContent],
   templateUrl: './multi-slot.html',
   styleUrl: './multi-slot.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MultiSlot {
-
-}
+export class MultiSlot {}
--- src/app/pages/single-slot/single-slot.ts
+++ src/app/pages/single-slot/single-slot.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Container } from '@features/simple-content-projection/container/container';
+import { ProjectedContent } from '@features/simple-content-projection/projected-content/projected-content';
 import { PageTitle } from '@ui/page-title/page-title';
-import { Container } from './container/container';
-import { ProjectedContent } from './projected-content/projected-content';
 
 @Component({
   selector: 'aor-single-slot',
